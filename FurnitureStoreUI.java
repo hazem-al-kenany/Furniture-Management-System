4b4703c5c366cef8467d4874b61b126f7c7b7da8
hazemelknany03 <126080162+hazemelknany03@users.noreply.github.com>
2023-12-15 23:47:22 +0200

    Task4

--- FurnitureStoreUI.java
+++ FurnitureStoreUI.java
@@ -1,4 +1,7 @@
 import java.util.*;
+import java.util.Date;
+import java.text.SimpleDateFormat;
+import java.text.ParseException;
 /**
  * Write a description of class FurnitureStoreUI here.
  * 
@@ -12,11 +15,11 @@ public class FurnitureStoreUI {
     private FURNITUREInterface store = new FurnitureStore("Fortune Furnitures", "Downtown");
 
     public void runUI() {
-	
-	
+    
+    
         int choice = getOption();
 
-	 // process choice
+     // process choice
         while (choice != 0) {
             switch (choice) {
                 case 1:
@@ -41,7 +44,7 @@ public class FurnitureStoreUI {
                     System.out.println("Invalid choice.");
             }
 
-	    // output menu & get choice
+        // output menu & get choice
             choice = getOption();
         }
         System.out.println("\nThank you for using Fortune Furnitures System.");
@@ -59,7 +62,7 @@ public class FurnitureStoreUI {
         System.out.print("Enter your choice: ");
         
 
-	System.out.println("Enter your choice");
+    System.out.println("Enter your choice");
         // read choice
         int option = reader.nextInt();
         reader.nextLine();
@@ -67,27 +70,115 @@ public class FurnitureStoreUI {
     }
 
     private void addFurniture() {
-       
+        System.out.println("Enter furniture details:");
+        System.out.print("ID: ");
+        String id = reader.nextLine();
+        System.out.print("Category: ");
+        String category = reader.nextLine();
+        System.out.print("Item Count: ");
+        int itemCount = reader.nextInt();
+        System.out.print("Price: ");
+        double price = reader.nextDouble();
+        reader.nextLine(); // Consume newline
+        System.out.print("History: ");
+        String history = reader.nextLine();
+
+        PassFurnitureBase furniture = new PassFurnitureBase(id, category, itemCount, price, history);
+        store.addFurniture(furniture);
+        System.out.println("Furniture added successfully!");
     }
 
     private void addPurchaser() {
-       
+        System.out.println("Enter purchaser details:");
+        System.out.print("ID: ");
+        String id = reader.nextLine();
+        System.out.print("Name: ");
+        String name = reader.nextLine();
+        System.out.print("Furniture Type: ");
+        String furnitureType = reader.nextLine();
+        System.out.print("Contact Details: ");
+        String contactDetails = reader.nextLine();
+        System.out.print("Purchase Date (dd-MM-yyyy): ");
+        String purchaseDateString = reader.nextLine();
+
+        // Date input
+        Date purchaseDate = null;
+        try {
+            purchaseDate = new SimpleDateFormat("dd-MM-yyyy").parse(purchaseDateString);
+        } catch (ParseException e) {
+            e.printStackTrace(); //this had to be added to fix compilation errors
+        }
+
+        Purchaser purchaser = new Purchaser(id, name, furnitureType, contactDetails, purchaseDate);
+        store.addPurchaser(purchaser);
+        System.out.println("Purchaser added successfully!");
     }
 
     private void addCategory() {
-      
+        System.out.println("Enter category details:");
+        System.out.print("ID: ");
+        String id = reader.nextLine();
+        System.out.print("Name: ");
+        String name = reader.nextLine();
+        System.out.print("Discount: ");
+        double discount = reader.nextDouble();
+        System.out.print("Availability (true/false): ");
+        boolean available = reader.nextBoolean();
+        reader.nextLine(); // Consume newline
+        System.out.print("Supplier ID: ");
+        int supplierId = reader.nextInt();
+        reader.nextLine(); // Consume newline
+        System.out.print("Order ID: ");
+        String orderId = reader.nextLine();
+        System.out.print("Quantity: ");
+        int quantity = reader.nextInt();
+
+        // Retrieve supplier and furniture category
+        Purchaser supplier = store.getPurchaserByID(supplierId); //returns the purchaser linked with the id (From the array list)
+        FurnitureCategory category = new FurnitureCategory(id, name, discount, available, supplier, orderId, quantity);
+        store.addCategory(category);
+        System.out.println("Category added successfully!");
     }
 
     private void viewFurniture() {
-       
+        System.out.print("Enter furniture ID: ");
+        int furnitureId = reader.nextInt();
+        PassFurnitureBase furniture = store.getFurnitureByID(furnitureId);
+        if (furniture != null) {
+            System.out.println("Furniture Details:");
+            System.out.println(furniture.toString());
+        } else {
+            System.out.println("Furniture not found!");
+        }
     }
 
     private void viewPurchaser() {
-       
+       System.out.println("Enter Purchaser ID: ");
+       int purchaserID = reader.nextInt();
+       Purchaser purchaser = store.getPurchaserByID(purchaserID);
+       if (purchaser != null) {
+           System.out.println("Purchaser Details:");
+           System.out.println("Name: " + purchaser.getName());
+           System.out.println("ID: " + purchaser.getID());
+           System.out.println("Contact Details: " + purchaser.getContactDetails());
+       }
+       else {
+           System.out.println("Purchaser not found!");
+       }
     }
 
     private void viewCategory() {
-       
+        System.out.print("Enter category ID: ");
+        int categoryId = reader.nextInt();
+        FurnitureCategory category = store.getCategoryByID(categoryId);
+        if (category != null) {
+            System.out.println("Category Details:");
+            System.out.println("Name: " + category.getTypeName());
+            System.out.println("ID: " + category.getID());
+            System.out.println("Max Load: " + category.getMaxLoad());
+        } else {
+            System.out.println("Category not found!");
+        }
     }
 
     public static void main(String[] args) {
